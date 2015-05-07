cc576e813c400c4e18445db784f55a8ecbb5070f
Nicolas Tremblay <ns.tremblay@gmail.com>
2015-05-07 14:10:43 -0600

    testing with error

--- lib/Math.pm
+++ lib/Math.pm
@@ -10,11 +10,11 @@ sub compute {
   my ($operator, $x, $y) = @_;
  
   if ($operator eq '+') {
-      return $x + $y;
+      return $x + $y + 1;
   } elsif ($operator eq '-') {
-      return $x - $y;
+      return $x - $y + 1;
   } elsif ($operator eq '*') {
-      return $x - $y;
+      return $x - $y+1;
   }
 }
  
